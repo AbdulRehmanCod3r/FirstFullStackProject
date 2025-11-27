3636cb675cb337dd1233cddd1749c7ca5f309e58
Affan Ahmed <ahmedaffar820@gmail.com>
2025-11-27 11:46:56 +0500

    feature : connection string

--- backend/NortwindSol/Nortwind.Api/Constants/NorthwindDatabase.cs
+++ backend/NortwindSol/Nortwind.Api/Constants/NorthwindDatabase.cs
@@ -2,6 +2,6 @@
 {
     public static class NorthwindDatabase
     {
-        public const string ConnectionString = "NorthwindDb";
+        public const string ConnectionString = "\"Data Source=.\\\\SQLEXPRESS;Initial Catalog=NorthwindDb;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
     }
 }
