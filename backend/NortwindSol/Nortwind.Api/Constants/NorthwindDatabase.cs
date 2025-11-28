085af20ee929be2b9057c5339d76a439c22ebdc5
Abdul Rehman <studyemail1712@gmail.com>
2025-11-28 10:36:00 +0500

    connection string

---
+++ backend/NortwindSol/Nortwind.Api/Constants/NorthwindDatabase.cs
@@ -0,0 +1,7 @@
+﻿namespace Nortwind.Api.Constants
+{
+    public static class NorthwindDatabase
+    {
+        public static string ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=NorthwindDb;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+    }
+}
